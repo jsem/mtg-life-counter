cf6c7952f0968d885eea284d9969635e9ee27814
jsem <james.robert.semple@gmail.com>
2018-05-26 01:48:01 +1000

    Added curly braces for app navigator import

--- app/App.js
+++ app/App.js
@@ -3,7 +3,7 @@ import { StyleSheet, View } from 'react-native';
 
 import KeepAwake from 'react-native-keep-awake';
 
-import AppNavigator from './config/routes';
+import { AppNavigator } from './config/routes';
 
 /**
  * Main app class. Instantiates the navigation stack
