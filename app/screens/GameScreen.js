950ac0453c35e4c392a27dafcfea9ac083ec37f3
jsem <james.robert.semple@gmail.com>
2019-01-25 00:02:46 +1100

    added menu button to game screen

--- app/screens/GameScreen.js
+++ app/screens/GameScreen.js
@@ -3,14 +3,21 @@ import { View } from 'react-native';
 
 import Orientation from 'react-native-orientation';
 import { connect } from 'react-redux';
+import { ScaledSheet } from 'react-native-size-matters';
 
+import IconButton from '../components/IconButton';
 import PlayerArea from '../components/PlayerArea';
+import { colourGrey } from '../config/colours';
 import { globalStyles } from '../config/styles';
 
 /**
  * Game screen. Displays player areas for each player and a central menu
  */
 class GameScreen extends Component {
+
+    showMenu = () => {
+        //TODO actually implement this
+    }
 
     renderPlayerAreas = () => {
         switch (this.props.game.numberPlayers) {
@@ -79,15 +86,31 @@ class GameScreen extends Component {
                 );
         }
     }
-
+    
     render () {
         return (
             <View style={globalStyles.containerScreenVertical}>
                 {this.renderPlayerAreas()}
+                <View style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center'}}>
+                <IconButton
+                    icon="th-large"
+                    onPress={this.showMenu}
+                    buttonStyle={styles.menuButton}
+                    iconStyle={globalStyles.text}
+                />
+                </View>
             </View>
         );
     }
 }
+
+const styles = ScaledSheet.create({
+    menuButton: {
+        backgroundColor: colourGrey,
+        height: '40@ms',
+        width: '40@ms'
+    }
+})
 
 export default GameScreen = connect(
     state => ({
